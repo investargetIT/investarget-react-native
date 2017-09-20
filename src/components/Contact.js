830dc7e8b49a947d755ec753e0cdc86a7445ba6f
Peter Xu <xzm920@163.com>
2017-09-20 13:25:37 +0800

    联系我们

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,27 +1,67 @@
 import React from 'react'
-import { Image, Text, View, StatusBar } from 'react-native'
+import { View, Text } from 'react-native'
+
+
+const containerStyle = {
+    backgroundColor: '#fff'
+}
+const cellStyle = {
+    flexDirection:'row',
+    alignItems:'center',
+    marginLeft: 16,
+    height: 40,
+    borderBottomColor: '#f4f4f4',
+    borderBottomWidth: 1
+}
+const cellLabelStyle = {
+    width: 80,
+    flex: 0,
+    fontSize: 13,
+    color: '#333'
+}
+const cellContentStyle = {
+    flex: 1,
+    fontSize: 13
+}
+
 
 class Contact extends React.Component {
-    
-    static navigationOptions = {
-        title: '联系我们',
-        headerStyle: {
-            backgroundColor: '#10458f',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-            // display: 'flex',
-            // flexDirection: 'row',
-            // alignItems: 'center',
-            // justifyContent: 'center',
+
+    static navigationOptions = ({ navigation }) => {
+        const { params } = navigation.state
+        return {
+            title: '联系我们',
+            headerTintColor: '#fff',
+            headerStyle: {
+                height: 48,
+                backgroundColor: '#10458F',
+            },
         }
+    }
+
+    constructor(props) {
+        super(props)
     }
 
     render() {
         return (
-            <View>
-                <StatusBar backgroundColor="red" barStyle="light-content" />
-                <Text>联系方式</Text>
+            <View style={containerStyle}>
+                <Cell label="客服电话：" content="021-31776196" />
+                <Cell label="客服邮箱：" content="customer@investarget.com" />
+                <Cell label="联系地址：" content="中国上海市徐汇区虹桥路777号17楼7单元" />
+            </View>
+        )
+    }
+}
+
+class Cell extends React.Component {
+
+    render() {
+        const { label, content } = this.props
+        return (
+            <View style={cellStyle}>
+                <Text style={cellLabelStyle}>{label}</Text>
+                <Text style={cellContentStyle}>{content}</Text>
             </View>
         )
     }
