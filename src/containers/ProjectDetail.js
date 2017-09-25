8bdfa401bfe1d02bdeba486321020b3e122a5236
Oliver <olimovier@gmail.com>
2017-09-25 11:50:42 +0800

    增加项目详情页的操作底栏

--- src/containers/ProjectDetail.js
+++ src/containers/ProjectDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, Text, View, StatusBar, WebView, Platform } from 'react-native';
+import { Image, Text, View, StatusBar, WebView, Platform, TouchableOpacity } from 'react-native';
 import * as api from '../api';
 
 class ProjectDetail extends React.Component {
@@ -26,9 +26,47 @@ class ProjectDetail extends React.Component {
       .then(token => this.setState({ url: `http://192.168.1.113:3000/project_for_rn/${this.id}?token=${token}` }))
     }
 
+    handleTimelinePress() {
+      console.log('Timeline press')
+    }
+
+    handleFavoritePress() {
+      console.log('Favorite press')
+    }
+
+    handleSavePress() {
+      console.log('save')
+    }
+    
     render() {
       if (!this.state.url) return null;
-      return <WebView source={{ uri: this.state.url }} />
+      return (
+        <View style={{ flex: 1 }}>
+
+          <WebView source={{ uri: this.state.url }} />
+
+          <View style={{ height: 42, backgroundColor: '#10458F', flexDirection: 'row' }}>
+            <View style={{ flex: 0.5, justifyContent: 'center', alignItems: 'center' }}>
+              <TouchableOpacity onPress={this.handleTimelinePress}>
+                <Text style={{ color: 'white', fontSize: 16 }} >时间轴</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={{ flex: 0.5, justifyContent: 'center', alignItems: 'center' }}>
+              <TouchableOpacity onPress={this.handleFavoritePress}>
+                <Text style={{ color: 'white', fontSize: 16 }}>感兴趣</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={{ position: 'absolute', left: 0, right: 0, bottom: 16, alignItems: 'center' }}>
+              <TouchableOpacity onPress={this.handleSavePress}>
+                <Image source={require('../images/home/projNoCollect.png')} style={{ width: 42, height: 42 }} />
+              </TouchableOpacity>
+            </View>
+          </View>
+
+        </View>
+      )
     }
 }
 
