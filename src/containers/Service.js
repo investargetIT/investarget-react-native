de7add34ba4b091f67edcf7c51c2115268fd33f7
Oliver <olimovier@gmail.com>
2017-10-17 16:20:01 +0800

    Little adjustment

--- src/containers/Service.js
+++ src/containers/Service.js
@@ -50,6 +50,8 @@ class Service extends React.Component {
             title: '选择名片',
             cancelButtonTitle: '取消',
             mediaType: 'photo',
+            takePhotoButtonTitle: '拍照',
+            chooseFromLibraryButtonTitle: '从相册中选择',
         }
         ImagePicker.showImagePicker(options, (response) => {
             if (response.didCancel) {
