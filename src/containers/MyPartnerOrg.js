bf6331c14fdf4a9bf384e76ecfbc6c9e60f04362
Oliver <olimovier@gmail.com>
2017-10-24 16:09:17 +0800

    调用接口以结构的形式展示我的投资人

--- src/containers/MyPartnerOrg.js
+++ src/containers/MyPartnerOrg.js
@@ -4,12 +4,15 @@ import {
   Text,
   FlatList,
 } from 'react-native';
+import * as api from '../api';
+import { connect } from 'react-redux';
 
-function MyPartnerOrgCell() {
+function MyPartnerOrgCell (props) {
+  const { org, count, data } = props.data;
   return (
     <View style={{ padding: 14 }}>
-      <Text style={{ fontSize: 16, color: 'black' }}>同学 (18)</Text>
-      <Text style={{ fontSize: 14, marginTop: 4,  color: 'grey' }}>章三, 里斯, 王五, 刘人</Text>
+      <Text style={{ fontSize: 16, color: 'black' }}>{org} ({count})</Text>
+      <Text numberOfLines={1} ellipsizeMode={'tail'} style={{ fontSize: 14, marginTop: 4,  color: 'grey' }}>{data.map(m => m.investoruser.username).join(', ')}</Text>
     </View>
   )
 }
@@ -23,18 +26,50 @@ class MyPartnerOrg extends React.Component {
         headerTintColor: '#fff',
     }
 
+    constructor (props) {
+      super(props);
+      this.state = {
+        data: []
+      }
+    }
+
+    componentDidMount() {
+      let org = [];
+      api.getOrg({ trader: this.props.userInfo.id, page_size: 10000 })
+      .then(data => {
+        org = data.data;
+        return Promise.all(
+          org.map(item => api.getUserRelation({ traderuser: this.props.userInfo.id, orgs: item.id, page_size: 10 }))
+        )
+      })
+      .then(data => {
+        data.forEach(function(element, index) {
+          element['id'] = org[index]['id'];
+          element['org'] = org[index]['orgname'];
+        }, this);
+        this.setState({ data })
+      })
+      .catch(err => console.error(err));
+    }
+
     separator = () => <View style={{ height: 0.3, backgroundColor: "#CED0CE", marginLeft: 10 }} />;
 
     render() {
       return (
         <FlatList
           style={{ backgroundColor: 'white' }}
-          data={[{ key: 'a' }, { key: 'b' }]}
-          renderItem={({ item }) => <MyPartnerOrgCell />}
+          data={this.state.data}
+          keyExtractor={ item => item.id }
+          renderItem={({ item }) => <MyPartnerOrgCell data={item} />}
           ItemSeparatorComponent={this.separator}
         />
       );
     }
 }
 
-export default MyPartnerOrg;
+function mapStateToProps(state) {
+  const { userInfo } = state.app;
+  return { userInfo };
+}
+
+export default connect(mapStateToProps)(MyPartnerOrg);
