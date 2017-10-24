1e23faa74752f9c97410dd69a9e8171f867ccbab
Oliver <olimovier@gmail.com>
2017-10-24 12:07:08 +0800

    使用静态数据绘制了基本页面

--- src/containers/MyPartnerOrg.js
+++ src/containers/MyPartnerOrg.js
@@ -1,5 +1,18 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { 
+  View, 
+  Text,
+  FlatList,
+} from 'react-native';
+
+function MyPartnerOrgCell() {
+  return (
+    <View style={{ padding: 14 }}>
+      <Text style={{ fontSize: 16, color: 'black' }}>同学 (18)</Text>
+      <Text style={{ fontSize: 14, marginTop: 4,  color: 'grey' }}>章三, 里斯, 王五, 刘人</Text>
+    </View>
+  )
+}
 
 class MyPartnerOrg extends React.Component {
     static navigationOptions = {
@@ -10,9 +23,18 @@ class MyPartnerOrg extends React.Component {
         headerTintColor: '#fff',
     }
 
-  render () {
-    return <Text>MyPartnerOrg</Text>;
-  }
+    separator = () => <View style={{ height: 0.3, backgroundColor: "#CED0CE", marginLeft: 10 }} />;
+
+    render() {
+      return (
+        <FlatList
+          style={{ backgroundColor: 'white' }}
+          data={[{ key: 'a' }, { key: 'b' }]}
+          renderItem={({ item }) => <MyPartnerOrgCell />}
+          ItemSeparatorComponent={this.separator}
+        />
+      );
+    }
 }
 
 export default MyPartnerOrg;
