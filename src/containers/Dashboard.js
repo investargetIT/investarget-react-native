3030270def8de8b7dfda6a16898aa0244e6c814e
Oliver <olimovier@gmail.com>
2018-07-30 18:31:48 +0800

    加载当前用户有机构BD的项目列表

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -13,7 +13,8 @@ class Dashboard extends React.Component {
     this.state = {
       fund: null,
       lc: null,
-      lp: null
+      lp: null,
+      list: [],
     };
   }
 
@@ -24,30 +25,34 @@ class Dashboard extends React.Component {
       .then(result => this.setState({ lc: result.count }));
     api.getOrg({ trader: this.props.userInfo.id, tags: 57 })
       .then(result => this.setState({ lp: result.count }));
+
+    api.getProj({ bdm: this.props.userInfo.id, projstatus: [4, 6, 7], page_size: 2 })
+      .then(result => this.setState({ list: result.data }));
   }
 
   render () {
     return (
       <View>
 
-      <View style={{ height: 120, flexDirection: 'row', backgroundColor: 'white' }}>
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: undefined }}>
-          <View><Text style={{ color: '#10458F' }}>基金</Text></View>
-          <View><Text style={{ fontSize: 30, color: '#10458F' }}>{this.state.fund}</Text></View>
+        <View style={{ height: 120, flexDirection: 'row', backgroundColor: 'white' }}>
+          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: undefined }}>
+            <View><Text style={{ color: '#10458F' }}>基金</Text></View>
+            <View><Text style={{ fontSize: 30, color: '#10458F' }}>{this.state.fund}</Text></View>
+          </View>
+          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: undefined }}>
+            <View><Text style={{ color: '#10458F' }}>上市公司</Text></View>
+            <View><Text style={{ fontSize: 30, color: '#10458F' }}>{this.state.lc}</Text></View>
+          </View>
+          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: undefined }}>
+            <View><Text style={{ color: '#10458F' }}>LP</Text></View>
+            <View><Text style={{ fontSize: 30, color: '#10458F' }}>{this.state.lp}</Text></View>
+          </View>
         </View>
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: undefined }}>
-          <View><Text style={{ color: '#10458F' }}>上市公司</Text></View>
-          <View><Text style={{ fontSize: 30, color: '#10458F' }}>{this.state.lc}</Text></View>
-        </View>
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: undefined }}>
-          <View><Text style={{ color: '#10458F' }}>LP</Text></View>
-          <View><Text style={{ fontSize: 30, color: '#10458F' }}>{this.state.lp}</Text></View>
-        </View>
-      </View>
 
         <View style={{ marginTop: 10 }}>
 
-          <View style={{ padding: 10, backgroundColor: 'white' }}>
+          { this.state.list.map(m => 
+          <View key={m.id} style={{ padding: 10, backgroundColor: 'white', borderBottomWidth: 0.3, borderBottomColor: '#CED0CE' }}>
 
             <View style={{ width: '80%', alignSelf: 'center', alignItems: 'center', backgroundColor: undefined }}>
               <Text style={{ marginBottom: 20, lineHeight: 20, textAlign: 'center', fontSize: 15 }}>灵芝项目：全新靶点多肽类抗肿瘤、脑卒中创新药研发企业</Text>
@@ -62,7 +67,7 @@ class Dashboard extends React.Component {
               <Text style={{ color: '#10458F' }}>项目详情</Text>
             </View>
 
-          </View>
+          </View>)}
 
         </View>
 
