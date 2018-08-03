73320168f02a895b24c2cc290e62b84b330a10be
Oliver <olimovier@gmail.com>
2018-08-03 16:27:35 +0800

    Dashboard页面载入所有活跃用户是负责人的机构BD

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -3,10 +3,43 @@ import {
   Text,
   View,
   TouchableOpacity,
-  TouchableHighlight
+  TouchableHighlight,
+  FlatList,
+  RefreshControl,
+  Image,
+  ActivityIndicator,
 } from 'react-native';
 import { connect } from 'react-redux';
 import * as api from '../api';
+
+const PAGE_SIZE = 10;
+
+function OrgBDGroupByProjCell (props) {
+  const { onPress, data, projOnPress } = props;
+  const { projtitle, country, transactionType, tags, orgCount } = data;
+  return (
+    <TouchableHighlight underlayColor="lightgray" onPress={onPress}>
+      <View style={{ backgroundColor: 'white' }}>
+
+        <View style={{ width: '80%', alignSelf: 'center', alignItems: 'center', backgroundColor: undefined }}>
+          <Text style={{ marginTop: 20, marginBottom: 20, lineHeight: 20, textAlign: 'center', fontSize: 15 }}>{projtitle}</Text>
+          <Text style={{ margin: 2, fontSize: 13, color: '#666' }}>地区：{country.country}</Text>
+          <Text style={{ margin: 2, fontSize: 13, color: '#666' }}>交易类型：{transactionType.map(m => m.name).join('、')}</Text>
+          <Text style={{ margin: 2, fontSize: 13, color: '#666', textAlign: 'center' }}>标签：{tags.map(m => m.name).join('、')}</Text>
+          {/* <Text style={{ margin: 10, lineHeight: 30, color: '#10458F', backgroundColor: undefined }}>点击查看时间轴</Text> */}
+        </View>
+
+        <View style={{ margin: 10, marginTop: 20, marginBottom: 20, height: undefined, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', backgroundColor: undefined }}>
+          <Text>BD机构：{orgCount}</Text>
+          <TouchableOpacity onPress={projOnPress}>
+            <Text style={{ color: '#10458F' }}>项目详情</Text>
+          </TouchableOpacity>
+        </View>
+
+      </View>
+    </TouchableHighlight>
+  );
+}
 
 class Dashboard extends React.Component {
 
@@ -17,6 +50,9 @@ class Dashboard extends React.Component {
       lc: null,
       lp: null,
       list: [],
+      loading: false,
+      isLoadingMore: false,
+      isLoadingAll: false,
     };
   }
 
@@ -27,9 +63,32 @@ class Dashboard extends React.Component {
       .then(result => this.setState({ lc: result.count }));
     api.getOrg({ trader: this.props.userInfo.id, tags: 57 })
       .then(result => this.setState({ lp: result.count }));
+    this.getData();
+  }
 
-    api.getProj({ bdm: this.props.userInfo.id, projstatus: [4, 6, 7], page_size: 2 })
-      .then(result => this.setState({ list: result.data }));
+  getData = async isLoadingMore => {
+    if (isLoadingMore === undefined) {
+      this.setState({ loading: true });
+    }
+    const allData = await api.getProj({ 
+      bdm: this.props.userInfo.id, 
+      projstatus: [4, 6, 7], 
+      skip_count: isLoadingMore ? this.state.list.length : 0, 
+      max_size: PAGE_SIZE 
+    });
+
+    for (let index = 0; index < allData.data.length; index++) {
+      const element = allData.data[index];
+      const bdReq = await api.getOrgBdBase({ proj: element.id, manager: this.props.userInfo.id });
+      element.orgCount = bdReq.count;
+    }
+
+    this.setState({ 
+      list: isLoadingMore ? this.state.list.concat(allData.data) : allData.data, 
+      loading: false, 
+      isLoadingAll: allData.data.length < PAGE_SIZE,
+      isLoadingMore: false,
+    });
   }
 
   handleItemPressed = item => this.props.navigation.navigate('ProjectDetail', { project: item });
@@ -56,59 +115,85 @@ class Dashboard extends React.Component {
     this.props.navigation.navigate('OrgBDList', { proj });
   }
 
+  separator = () => <View style={{ height: 0.5, backgroundColor: "#CED0CE", marginLeft: undefined }} />;
+
+  loadMore = () => {
+    if (this.state.isLoadingAll || this.state.isLoadingMore || this.state.list.length === 0) return;
+    this.setState({ isLoadingMore: true });
+    this.getData(true);
+  }
+
+  renderFooter = () => {
+    if (this.state.list.length < PAGE_SIZE) return null;
+    if (this.state.isLoadingAll) return (
+      <Text style={{ padding: 16, textAlign: 'center', fontSize: 12, color: 'gray' }}>---没有更多了---</Text>
+    );
+
+    return (
+      <View
+        style={{
+          paddingVertical: 20,
+          borderColor: "#CED0CE"
+        }}>
+        <ActivityIndicator animating size="small" />
+      </View>
+    );
+  };
+
+  renderHeader = () => (
+    <View>
+      <View style={{ height: 120, flexDirection: 'row', backgroundColor: 'white' }}>
+        <TouchableHighlight underlayColor="lightgray" onPress={this.handleOrgTypePressed.bind(this, 1)} style={{ flex: 1 }}>
+          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white' }}>
+            <View><Text style={{ color: '#10458F' }}>基金</Text></View>
+            <View><Text style={{ fontSize: 30, color: '#10458F' }}>{this.state.fund}</Text></View>
+          </View>
+        </TouchableHighlight>
+        <TouchableHighlight underlayColor="lightgray" onPress={this.handleOrgTypePressed.bind(this, 12)} style={{ flex: 1 }}>
+          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white' }}>
+            <View><Text style={{ color: '#10458F' }}>上市公司</Text></View>
+            <View><Text style={{ fontSize: 30, color: '#10458F' }}>{this.state.lc}</Text></View>
+          </View>
+        </TouchableHighlight>
+        <TouchableHighlight underlayColor="lightgray" onPress={this.handleOrgTypePressed.bind(this, 57)} style={{ flex: 1 }}>
+          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white' }}>
+            <View><Text style={{ color: '#10458F' }}>LP</Text></View>
+            <View><Text style={{ fontSize: 30, color: '#10458F' }}>{this.state.lp}</Text></View>
+          </View>
+        </TouchableHighlight>
+      </View>
+      <View style={{ height: 5 }} />
+    </View>
+  );
 
   render () {
     return (
-      <View>
-
-        <View style={{ height: 120, flexDirection: 'row', backgroundColor: 'white' }}>
-          <TouchableHighlight underlayColor="lightgray" onPress={this.handleOrgTypePressed.bind(this, 1)} style={{ flex: 1 }}>
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white' }}>
-              <View><Text style={{ color: '#10458F' }}>基金</Text></View>
-              <View><Text style={{ fontSize: 30, color: '#10458F' }}>{this.state.fund}</Text></View>
-            </View>
-          </TouchableHighlight>
-          <TouchableHighlight underlayColor="lightgray" onPress={this.handleOrgTypePressed.bind(this, 12)} style={{ flex: 1 }}>
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white' }}>
-              <View><Text style={{ color: '#10458F' }}>上市公司</Text></View>
-              <View><Text style={{ fontSize: 30, color: '#10458F' }}>{this.state.lc}</Text></View>
-            </View>
-          </TouchableHighlight>
-          <TouchableHighlight underlayColor="lightgray" onPress={this.handleOrgTypePressed.bind(this, 57)} style={{ flex: 1 }}>
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white' }}>
-              <View><Text style={{ color: '#10458F' }}>LP</Text></View>
-              <View><Text style={{ fontSize: 30, color: '#10458F' }}>{this.state.lp}</Text></View>
-            </View>
-          </TouchableHighlight>
-        </View>
-
-        <View style={{ marginTop: 10 }}>
-
-          {this.state.list.map(m =>
-            <TouchableHighlight key={m.id} underlayColor="lightgray" onPress={this.handleProjPressed.bind(this, m)}>
-              <View style={{ padding: 10, backgroundColor: 'white', borderBottomWidth: 0.3, borderBottomColor: '#CED0CE' }}>
-
-                <View style={{ width: '80%', alignSelf: 'center', alignItems: 'center', backgroundColor: undefined }}>
-                  <Text style={{ marginBottom: 20, lineHeight: 20, textAlign: 'center', fontSize: 15 }}>{m.projtitle}</Text>
-                  <Text style={{ margin: 2, fontSize: 13, color: '#666' }}>地区：{m.country.country}</Text>
-                  <Text style={{ margin: 2, fontSize: 13, color: '#666' }}>交易类型：{m.transactionType.map(m => m.name).join('、')}</Text>
-                  <Text style={{ margin: 2, fontSize: 13, color: '#666', textAlign: 'center' }}>标签：{m.tags.map(m => m.name).join('、')}</Text>
-                  <Text style={{ margin: 20, lineHeight: 30, color: '#10458F', backgroundColor: undefined }}>点击查看时间轴</Text>
-                </View>
-
-                <View style={{ height: undefined, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', backgroundColor: undefined }}>
-                  <Text>BD投资人：10</Text>
-                  <TouchableOpacity onPress={this.handleItemPressed.bind(this, m)}>
-                    <Text style={{ color: '#10458F' }}>项目详情</Text>
-                  </TouchableOpacity>
-                </View>
-
-              </View>
-            </TouchableHighlight>)}
-
-        </View>
-
-      </View>
+      <FlatList
+        style={{ backgroundColor: undefined }}
+        data={this.state.list}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => <OrgBDGroupByProjCell
+          onPress={this.handleProjPressed.bind(this, item)}
+          projOnPress={this.handleItemPressed.bind(this, item)}
+          data={item}
+        />}
+        ListHeaderComponent={this.renderHeader}
+        refreshControl={
+          <RefreshControl
+            refreshing={this.state.loading}
+            onRefresh={this.getData}
+            colors={['#10458f']}
+            tintColor="#10458f"
+          />
+        }
+        ItemSeparatorComponent={this.separator}
+        onEndReachedThreshold={0.01}
+        onEndReached={this.loadMore}
+        ListFooterComponent={this.renderFooter}
+        ListEmptyComponent={<View style={{ flex: 1, alignItems: 'center', paddingTop: 60 }}>
+          <Image style={{ width: 100, height: 86 }} source={require('../images/emptyBox.png')} />
+        </View>}
+      />
     );
   }
 }
