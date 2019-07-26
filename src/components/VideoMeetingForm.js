17f67501acbd90431e8f661e291a93ae9945c225
InvesTarget <system@investarget.com>
2019-07-26 13:19:42 +0800

    Remove unused components in video meeting form

--- src/components/VideoMeetingForm.js
+++ src/components/VideoMeetingForm.js
@@ -12,19 +12,9 @@ import {
     TimePickerAndroid,
 } from 'react-native';
 import ProjectItem from './ProjectItem';
-import UserItem from './UserItem';
-import Picker from './Picker';
-import Cascader from './Cascader';
 import { connect } from 'react-redux';
 import * as api from '../api';
 import { receiveContinentsAndCountries } from '../../actions';
-
-const scheduleTypeOptions = [
-    { label: '路演会议', value: 1 },
-    { label: '约见公司', value: 2 },
-    { label: '约见投资人', value: 3 },
-    { label: '视频会议', value: 4 },
-];
 
 class VideoMeetingForm extends React.Component {
 
@@ -90,15 +80,6 @@ class VideoMeetingForm extends React.Component {
             <View>
                 <View style={{ backgroundColor: 'white', marginTop: 20 }}>
 
-                    <Picker
-                        style={{ flex: 1, height: 44, paddingLeft: 2 }}
-                        value={props.type}
-                        onChange={props.handleChangeType}
-                        placeholder="日程类型"
-                        options={scheduleTypeOptions} />
-
-                    <View style={{ height: 0.4, backgroundColor: "#CED0CE", marginLeft: 10 }} />
-
                     <View style={{ height: 44, paddingLeft: 10, paddingRight: 10, justifyContent: 'center' }}>
                         <TextInput
                             style={{ fontSize: 16, paddingLeft: 0 }}
@@ -110,33 +91,6 @@ class VideoMeetingForm extends React.Component {
                     </View>
 
                     <View style={{ height: 0.4, backgroundColor: "#CED0CE", marginLeft: 10 }} />
-
-                    <TouchableHighlight
-                      style={{ backgroundColor: 'white' }}
-                      onPress={() => this.setState({ showSelectCountry: true })}
-                      underlayColor={'lightgray'}
-                    >
-                      <View style={{ height: 44, paddingLeft: 10, paddingRight: 10, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
-                        <Text style={{ fontSize: 16, color: props.country ? undefined : 'lightgray' }}>
-                          {props.country ? props.country.label : '国家'}
-                        </Text>
-                      </View>
-                    </TouchableHighlight>
-
-                    <View style={{ height: 0.4, backgroundColor: "#CED0CE", marginLeft: 10 }} />
-
-                    { isChina ? 
-                    <Picker
-                        style={{flex: 1, height: 44, paddingLeft: 2}}
-                        value={props.location}
-                        onChange={props.handleChangeArea}
-                        placeholder="地区"
-                        options={props.areaOptions} />
-                   : null }
-
-                    { isChina ? 
-                    <View style={{ height: 0.4, backgroundColor: "#CED0CE", marginLeft: 10 }} />
-                    : null }
 
                     <View style={{ height: 44, paddingLeft: 10, paddingRight: 10, justifyContent: 'center' }}>
                         <TextInput
@@ -160,33 +114,11 @@ class VideoMeetingForm extends React.Component {
                     </View>
                 </TouchableHighlight>
 
-                {props.project ?
-                    <View style={{ marginTop: 20 }}>
-                        <ProjectItem {...props.project} onPress={props.handleProjectPressed} />
-                    </View>
-                    :
-                    <TouchableHighlight
-                        style={{ marginTop: 20 }}
-                        underlayColor="lightgray"
-                        onPress={props.handleProjectPressed}
-                    >
-                        <View style={{ height: 44, paddingLeft: 10, paddingRight: 10, justifyContent: 'center', backgroundColor: 'white' }}>
-                            <Text style={{ fontSize: 16 }}>添加项目</Text>
-                        </View>
-                    </TouchableHighlight>
-                }
-
-                {props.user ?
-                    <View style={{ marginTop: 20 }}>
-                        <UserItem {...props.user} onSelect={props.handleUserPressed} />
-                    </View>
-                    :
-                    <TouchableHighlight style={{ marginTop: 20 }} underlayColor="lightgray" onPress={props.handleUserPressed}>
-                        <View style={{ height: 44, paddingLeft: 10, paddingRight: 10, justifyContent: 'center', backgroundColor: 'white' }}>
-                            <Text style={{ fontSize: 16 }}>添加用户</Text>
-                        </View>
-                    </TouchableHighlight>
-                }
+            {props.project &&
+              <View style={{ marginTop: 20 }}>
+                <ProjectItem {...props.project} onPress={props.handleProjectPressed} />
+              </View>
+            }
 
                 {Platform.OS === 'ios' && this.state.showDatePickerIOS ?
                     <Modal
@@ -208,24 +140,6 @@ class VideoMeetingForm extends React.Component {
                         </TouchableWithoutFeedback>
                     </Modal>
                     : null}
-
-                <Modal
-                  transparent={true}
-                  visible={this.state.showSelectCountry}
-                >
-                  <TouchableWithoutFeedback style={{ backgroundColor: 'cyan' }} onPress={() => this.setState({ showSelectCountry: false })}>
-                    <View style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, backgroundColor: 'rgba(0, 0, 0, .2)' }}>
-                      <View style={{ position: 'absolute', bottom: 0, left: 0, right: 0, height: '50%' }}>
-                        <Cascader
-                          chosenItem={[]}
-                          onItemClick={this.handleSelectCountry} 
-                          options={props.countryOptions} 
-                        />
-                      </View>
-                    </View>
-                  </TouchableWithoutFeedback>
-                </Modal>
-
             </View>
         );
     }
