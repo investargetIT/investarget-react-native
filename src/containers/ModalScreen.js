d3b97233657d6d5322283acbf9e5c5ecd91ec5d6
InvesTarget <system@investarget.com>
2019-08-06 10:09:06 +0800

    Save modal work

--- src/containers/ModalScreen.js
+++ src/containers/ModalScreen.js
@@ -1,23 +1,38 @@
 import React from 'react';
-import { View, Text, Button, TouchableOpacity, StatusBar } from 'react-native';
+import { View, Text, Button, TouchableOpacity, SafeAreaView, Platform, ScrollView, StatusBar } from 'react-native';
+import DeviceInfo from 'react-native-device-info';
+import CommonStatusBar from '../components/StatusBar';
 
 class ModalScreen extends React.Component {
+
+  componentDidMount() {
+    if (Platform.OS === 'android' && DeviceInfo.hasNotch()) {
+      SafeAreaView.setStatusBarHeight(
+        /* Some value for status bar height + notch height */
+      );
+    }
+  }
+
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <View style={{ paddingTop: 20, height: 88, flexDirection: 'row', justifyContent: 'space-between', backgroundColor: '#10458f' }}>
+      <CommonStatusBar backgroundColor="#10458f" barStyle="light-content" />
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={{ height: 44, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', backgroundColor: '#10458f' }}>
           <TouchableOpacity
             style={{ marginLeft: 16 }}
             onPress={this.handleCancelBtnPressed}>
             <Text style={{ fontSize: 15, color: 'white' }}>取消</Text>
           </TouchableOpacity>
-          <Text>标题</Text>
+          <Text style={{ fontSize: 16, color: 'white' }}>标题</Text>
           <TouchableOpacity
             style={{ marginRight: 16 }}
             onPress={this.handleSaveBtnPressed}>
             <Text style={{ fontSize: 15, color: 'white' }}>保存</Text>
           </TouchableOpacity>
         </View>
+        <ScrollView>
+          <View style={{ height: 1000 }} />
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
           <Text style={{ fontSize: 30 }}>This is a modal!</Text>
           <Button
@@ -25,6 +40,8 @@ class ModalScreen extends React.Component {
             title="Dismiss"
           />
         </View>
+        </ScrollView>
+      </SafeAreaView>
       </View>
     );
   }
