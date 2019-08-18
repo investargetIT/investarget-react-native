94920769153708c14c89545a721b100a73a605e8
InvesTarget <system@investarget.com>
2019-08-18 23:21:33 +0800

    Remove unused codes

--- src/components/ModifyProjBDStatus.js
+++ src/components/ModifyProjBDStatus.js
@@ -81,20 +81,11 @@ constructor(props){
 	    group: '',
 	    disabled: this.props.source === 'projectBD' ? false : true,
         visible: true,
-        confirmModal:false
 	}
 }
 
 setModalVisible = (visible) =>{              
 	this.props.setVisible(visible)    
-}
-
-pressCancel = () =>{
-    this.handleConfirmAudit(false)
-}
-
-pressOk = () =>{
-    this.handleConfirmAudit(true)
 }
 
 checkInvalid = () =>{
@@ -107,166 +98,17 @@ checkInvalid = () =>{
 }
 
 
-    wechatConfirm = () => {
-        const { bd_status, confirmModal } = this.state
-        const { currentBD } = this.props
-        if (bd_status.id == 3 && currentBD.bd_status.id != 3) {
-            if (!currentBD.bduser) {
-                this.checkExistence(this.state.mobile, this.state.email).then(ifExist => {
-                    if (ifExist) {
-                        Alert.alert('用户已存在');
-                    } else {
-                        this.handleConfirmAudit(true);
-                    }
-                })
-            } else {
-                // 已经有联系人时
-                if (currentBD.wechat && currentBD.wechat.length > 0) {
-                    // 该联系人已经有微信
-                    this.setState({ visible: false, confirmModal: true })
-                } else {
-                    // 该联系人没有微信
-                    this.setState({ visible: false }, () => this.handleConfirmAudit(true));
-                }
-            }
-        } else {
-            this.handleConfirmAudit(true)
-        }
-    }
-
-checkExistence = (mobile, email) => {
-    return new Promise((resolve, reject) => {
-      Promise.all([api.checkUserExist(mobile),api.checkUserExist(email)])
-        .then(result => {
-          for(let item of result) {
-            if(item.result === true)
-                resolve(true);
-          }
-          resolve(false);
-        })
-        .catch(err => reject(err));
-    });
-}
-
-handleConfirmAudit = (isModifyWechat) =>{
-    const {bd_status, username,mobile,wechat,email,group} = this.state
-    const {currentBD} = this.props
-    const body={bd_status:bd_status.id}
-    api.modifyOrgBD(currentBD.id, body).then(()=>{
-        DeviceEventEmitter.emit('updateOrgBD')
-        if(bd_status.id!==3 || currentBD.bd_status.id===3) {
-        this.setState({visible:false})
-        this.props.navigation.goBack()
-        }        
+  confirmModify = () => {
+    const { bd_status, username, mobile, wechat, email, group } = this.state
+    const { source, currentBD } = this.props
+    this.setModalVisible(false)
+    api.editProjBD(currentBD.id, { bd_status: bd_status.id }).then(() => {
+      DeviceEventEmitter.emit('updateProjBD')
+      this.props.navigation.goBack()
     }).catch(error => {
-        Toast.show(error.message, {position: Toast.positions.CENTER})
+      Toast.show(error.message, { position: Toast.positions.CENTER })
     })
-    
-    if(bd_status.id!==3 || currentBD.bd_status.id===3) {
-        return;
-    }
-
-    if(currentBD.bduser){
-
-      api.checkUserRelation(currentBD.bduser, currentBD.manager.id)
-        .then(result => {
-          if (result.data || hasPerm('usersys.admin_changeuser') && isModifyWechat) {
-            api.editUser([currentBD.bduser], { wechat }); 
-          } else {
-            api.addUserRelation({
-              relationtype: false,
-              investoruser: currentBD.bduser,
-              traderuser: currentBD.manager.id
-            })
-              .then(result => {
-                if (isModifyWechat) {
-                  api.editUser([currentBD.bduser], { wechat });
-                }
-              })
-              .catch(error => {
-                if (isModifyWechat && error.code === 2025) {
-                  Alert.alert('该用户正处于保护期，无法建立联系，因此暂时无法修改微信');
-                }
-              }); 
-          }
-        });
-
-        this.addRelation(currentBD.bduser);
-        
-        api.addOrgBDComment({
-        orgBD: currentBD.id,
-        comments: `微信: ${wechat}`
-      }).then((data)=>{
-        DeviceEventEmitter.emit('updateOrgBD') 
-        this.props.navigation.goBack()
-      })
-    }
-    else{
-        api.addOrgBDComment({
-        orgBD: currentBD.id,
-        comments: `姓名: ${username} 手机号码: ${mobile} 微信: ${wechat} 邮箱: ${email}`
-      });
-        const newUser = { mobile, wechat, email, groups: [Number(group)], userstatus: 2 };
-        if (window.LANG === 'en') {
-            newUser.usernameE = username;
-        } else {
-            newUser.usernameC = username;
-        }
-
-        this.checkExistence(mobile,email).then(ifExist=>{
-        if(ifExist){
-            alert('用户已存在')         
-        }
-        else{
-        this.setState({visible:false})
-        api.addUser(newUser)
-        .then(result =>{
-          api.addUserRelation({
-          relationtype: false,
-          investoruser: result.id,
-          traderuser: currentBD.manager.id
-          }).then(data=>{
-            DeviceEventEmitter.emit('updateOrgBD') 
-            this.props.navigation.goBack()
-          })
-        })
-        }
-        });
-            
-        
-        
-    }
-}
-
-addRelation = investorID =>{
-    const {currentBD} = this.props
-    if(currentBD.makeUser && currentBD.proj){
-        api.addUserRelation({
-        relationtype: false,
-        investoruser: investorID,
-        traderuser: currentBD.makeUser,
-        proj: currentBD.proj.id,
-      }) 
-       }
-}
-
-confirmModify = () =>{
-	const {bd_status, username,mobile,wechat,email,group} = this.state
-	const {source, currentBD} = this.props
-
-	if(source == 'orgBD'){
-        this.wechatConfirm()
-    }
-    else if(source == 'projectBD'){
-        this.setModalVisible(false)
-    	api.editProjBD(currentBD.id, { bd_status:bd_status.id }).then(()=>{
-			DeviceEventEmitter.emit('updateProjBD')
-			this.props.navigation.goBack()
-		}).catch(error => {
-            Toast.show(error.message, {position: Toast.positions.CENTER})
-        })
-    }
-}
+  }
 
 handleChangeStatus = value =>{
 	this.setState({bd_status:{name:this.props.statusOptions.find(item=>item.value==value).label, id:value}},this.checkInvalid)	
@@ -288,7 +130,7 @@ isShowContactForm = () => {
 }
 
 render(){
-	const {bd_status, group, disabled, confirmModal, visible} = this.state
+	const {bd_status, group, disabled, visible} = this.state
 	const {source, currentBD} = this.props
     let color=disabled ? 'white' : 'lightblue'
     buttonStyle={...buttonStyle,backgroundColor:color}
@@ -345,26 +187,6 @@ render(){
     	</View>	
     	</View>
     </View>
-    </Modal>
-    <Modal
-    animationType={"fade"}
-    transparent={true}
-    visible={confirmModal}>
-        <View style={backgroundStyle}>
-            <View style={modalStyle}>
-                <View style={{marginBottom:20}}>
-                    <Text style={{textAlign:'center'}}>联系人微信已存在，是否覆盖现有微信</Text>
-                </View>
-                <View style={{flex:0,flexDirection:'row',justifyContent:'center'}}>
-                <TouchableOpacity  style={{width:35,marginRight:20}} onPress={this.pressCancel.bind(this)}>
-                    <Text >取消</Text>
-                </TouchableOpacity>
-                <TouchableOpacity  style={{width:35}} onPress={this.pressOk.bind(this)}>
-                    <Text style={{color:'blue'}}>确认</Text>
-                </TouchableOpacity>
-                </View>
-            </View>
-        </View>
     </Modal>
     </View>
 	)
